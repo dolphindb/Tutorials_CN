02e4219a0986e2bb801cd4805adb784c7102ab9d
Yunan Gao <123862555+yunangao123@users.noreply.github.com>
2023-12-05 17:18:44 +0800

    Update cppApiDemo.cpp

--- script/vs2017/cppApiDemo.cpp
+++ script/vs2017/cppApiDemo.cpp
@@ -1,11 +1,14 @@
-﻿#include "BatchTableWriter.h"
+﻿// AppImportCsv.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "MultithreadedTableWriter.h"
 #include "DolphinDB.h"
 #include "Util.h"
 #include "rapidcsv.h"
 #include <string>
 #include <vector>
 #include <thread>
-
+#include "ScalarImp.h"
 using namespace std;
 using namespace dolphindb;
 
@@ -28,10 +31,48 @@ int main(int argc, char *argv[]) {
 	string userId = "admin";
 	string password = "123456";
 
-	string path = "d:/data/devices_big_readings.csv";
+	string path = "d:/data/devices_big/devices_big_readings.csv";
+	int days = Util::countDays(2023, 9, 22);
+	
+	Constant* p= Util::createTimestamp(2023, 9, 22, 9, 52, 34, 0);
+	Constant* seconds= Util::parseConstant(DT_TIMESTAMP, "2022.09.22T09:52:34");
+	cout << days << "," << seconds->getLong() <<","<< p->getLong()<<endl;
+	try {
+		//MultithreadedTableWriter(String hostName, int port, String userId, String password,
+		//String dbName, String tableName, boolean useSSL,
+		//	boolean enableHighAvailability, String[] highAvailabilitySites,
+		//	int batchSize, float throttle,
+		//	int threadCount, String partitionCol,
+		//	int[] compressTypes, Mode mode, String[] pModeOption, Callback callbackHandler)
+/*
+login(`admin, `123456)
+if (exists('dfs://iot') ) dropDatabase('dfs://iot')
 
-	try	{
-		dolphindb::BatchTableWriter btw(host, port, userId, password, true);
+db1 = database('',VALUE,2016.11.15..2016.11.18)
+db2 = database('',HASH,[SYMBOL,10])
+db = database('dfs://iot',COMPO,[db1,db2])
+
+schema=table(1:0,`time`device_id`battery_level`battery_status`battery_temperature`bssid`cpu_avg_1min`cpu_avg_5min`cpu_avg_15min`mem_free`mem_used`rssi`ssid,
+ [DATETIME,SYMBOL,INT,SYMBOL,DOUBLE,SYMBOL,DOUBLE,DOUBLE,DOUBLE,LONG,LONG,SHORT,SYMBOL])
+ db.createPartitionedTable(schema,`readings,`time`device_id)
+*/
+		vector<COMPRESS_METHOD> compress;
+		compress.push_back(COMPRESS_DELTA);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_DELTA);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_DELTA);
+		compress.push_back(COMPRESS_DELTA);
+		compress.push_back(COMPRESS_LZ4);
+		compress.push_back(COMPRESS_LZ4);
+
+		MultithreadedTableWriter writer("127.0.0.1", 8848, "admin", "123456", "dfs://iot", "readings",
+			false, false, NULL, 10000, 1, 10, "device_id", &compress);
 
 		rapidcsv::Document doc(path, rapidcsv::LabelParams(-1, -1));
 		std::vector<string> time = doc.GetColumn<string>(0);
@@ -49,32 +90,72 @@ int main(int argc, char *argv[]) {
 		std::vector<string> ssid = doc.GetColumn<string>(12);
 
 		int rowNum = time.size();
-		btw.addTable("dfs://iot", "readings");
+		//btw.addTable("dfs://iot", "readings");
+		ErrorCodeInfo errorInfo;
+
 
 		long long startTime = Util::getEpochTime();
-
-		for (int i = 0; i < rowNum; i++) {
-			btw.insert("dfs://iot", "readings",
-					   createDateTime(time[i]),
-					   device_id[i],
-					   battery_level[i],
-					   battery_status[i],
-					   battery_temperature[i],
-					   bssid[i],
-					   cpu_avg_1min[i],
-					   cpu_avg_5min[i],
-					   cpu_avg_15min[i],
-					   mem_free[i],
-					   mem_used[i],
-					   (short)rssi[i],
-					   ssid[i]);
+		
+		for (int i = 0; i < 100/*rowNum*/; i++) {
+			if (writer.insert(errorInfo, 
+				createDateTime(time[i]),
+				device_id[i],
+				battery_level[i],
+				battery_status[i],
+				battery_temperature[i],
+				bssid[i],
+				cpu_avg_1min[i],
+				cpu_avg_5min[i],
+				cpu_avg_15min[i],
+				mem_free[i],
+				mem_used[i],
+				(short)rssi[i],
+				ssid[i]
+				) == false) {
+				//此处不会执行到
+				cout << "insert failed: " << errorInfo.errorInfo << endl;
+				break;
+			}
+				
 		}
-
+		//检查目前MTW的状态
+		MultithreadedTableWriter::Status status;
+		writer.getStatus(status);
+		if (status.hasError()) {
+			cout << "error in writing: " << status.errorInfo << endl;
+		}
+		writer.waitForThreadCompletion();
+		//再次检查完成后的MTW状态
+		writer.getStatus(status);
+		if (status.hasError()) {
+			cout << "error after write complete: " << status.errorInfo << endl;
+			//获取未写入的数据
+			std::vector<std::vector<ConstantSP>*> unwrittenData;
+			writer.getUnwrittenData(unwrittenData);
+			cout << "unwriterdata length " << unwrittenData.size() << endl;
+		}
+			   		 
 		cout << "Insert Time " << Util::getEpochTime() - startTime << " ms" << endl;
+		//检查最后写入结果
+		cout << conn.run("select count(*) from pt")->getString() << endl;
 
-		btw.removeTable("dfs://iot", "readings");
+
 	}
 	catch (std::exception &e) {
 		cerr << "Failed to insert table, with exception: " << e.what() << endl;
 	}
+
+
+
 }
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
